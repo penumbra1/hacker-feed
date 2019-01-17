dccbc11dbbabdd802340ce4a380aae34b7c633fe
Olga Isakova <penumbral@yandex.ru>
2019-01-17 20:44:54 +0500

    apollo-link-context, authorization header

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,27 @@ import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import App from "./components/App";
+import { AUTH_TOKEN } from "./auth";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((req, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
